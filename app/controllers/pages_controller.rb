11efede6472ccf4cd09275912ceecbc6323aaaf4
vlzki <vlzki.dev@gmail.com>
2017-03-30 17:34:15 +0200

    added link tabs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :calendar, :contact ]
 
   def home
+  end
+
+  def calendar
+  end
+
+  def contact
   end
 end
