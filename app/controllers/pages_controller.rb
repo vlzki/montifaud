9c8ea6451e88301e0741d233dd0a10906f560c67
vlzki <vlzki.dev@gmail.com>
2017-05-02 20:44:24 +0200

    added other pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :calendar, :contact, :environnement ]
+  skip_before_action :authenticate_user!, only: [ :home, :calendar, :contact, :environnement, :jardin, :patrimoine, :repos, :gite, :chambre, :gastronomie, :sante, :valeurs, :art, :sens ]
 
   def home
   end
@@ -11,5 +11,35 @@ class PagesController < ApplicationController
   end
 
   def environnement
+  end
+
+  def jardin
+  end
+
+  def patrimoine
+  end
+
+  def repos
+  end
+
+  def gite
+  end
+
+  def chambre
+  end
+
+  def gastronomie
+  end
+
+  def sante
+  end
+
+  def valeurs
+  end
+
+  def art
+  end
+
+  def sens
   end
 end
